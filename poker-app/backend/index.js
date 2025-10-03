141914043cc29bff74bc73e217cb3a777252dbef
Amit Cohen <amit@Amits-MacBook-Air-2.local>
2025-10-03 19:17:29 +0300

    add proper gitignore

--- poker-app/backend/index.js
+++ poker-app/backend/index.js
@@ -57,4 +57,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
-app.listen(4000, () => console.log("✅ Backend running on http://localhost:4000"));
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`✅ Backend running on port ${PORT}`));
